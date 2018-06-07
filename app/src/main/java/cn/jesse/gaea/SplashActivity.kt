594f1dd6315c94afa1cd93f9d4ebbcab479af056
Jesse <lenjoyshow@gmail.com>
2018-06-07 20:45:06 +0800

    feat: create main bundle

---
+++ app/src/main/java/cn/jesse/gaea/SplashActivity.kt
@@ -0,0 +1,21 @@
+package cn.jesse.gaea
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.host_activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.host_activity_splash)
+
+        tvSplash.postDelayed({
+            val intent = Intent()
+            intent.setClassName(baseContext, "cn.jesse.gaea.app.main.MainActivity")
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
